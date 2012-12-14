f2b828935eb79fb0fddc8ab4e7feb7ab913ba7f2
Will Scott <willscott@gmail.com>
2012-12-13 19:37:19 -0800

    Send image over to phone

--- Setalyzer/src/com/quimian/setalyzer/FeatureTrainer.java
+++ Setalyzer/src/com/quimian/setalyzer/FeatureTrainer.java
@@ -23,6 +23,12 @@ public class FeatureTrainer extends AsyncTask<String, Void, Void> {
 			while (true) {
 				try {
 					SetCard sc = (SetCard)in.readObject();
+					Log.i("Setalyzer feature trainer", "card gotten");
+					Long length = (Long)in.readObject();
+					Log.i("Setalyzer feature trainer", "reading card data of length " + length);
+					byte[] image = new byte[length.intValue()];
+					in.readFully(image);
+					Log.i("setalyzer feature trainer", "Card read.");
 				} catch (EOFException e) {
 					continue;
 				}
